77134b8d56ad8407407bfca0422d4def50bed1bc
Brian7950 <brianh7950@gmail.com>
2023-05-08 22:39:38 -0600

    added pieRepo export

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ let pies = [
 ];
 //Create GET to return a list of all pies 
 router.get('/', function (req, res, next){
-    res.send(pies);
+    res.status(200).json({
+        "status": 200,
+        "statusText": "OK",
+        "message": "All pies retrieved",
+        "data": pies
+    });
 });
 
 // Configure router so all routes are prefixed with /api/v1
