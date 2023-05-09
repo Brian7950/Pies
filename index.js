457b994a82fb18e4eb70bdbdb7a16a15854e2f84
Brian7950 <brianh7950@gmail.com>
2023-05-08 23:55:49 -0600

    added assets directory, changed get method

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 //Bring in Express server and create application, 
 let express = require('express');
+let pieRepo = require('./repos/pieRepo')
 //we bring express() from express npm package, many other objects are created form this application object 
 let app = express();
 
 
 //Use the express Router method that is used to create new Router objects, router obj is collections of middlewares and routes
 let router = express.Router();
+// got object from module inside repos directory. 
 
-let pies = [
-    {"id": 1, "name": "Apple"},
-    {"id": 2, "name": "Cherry"},
-    {"id": 3, "name": "Peach"}, 
-];
+
 //Create GET to return a list of all pies 
 router.get('/', function (req, res, next){
+    pieRepo.get(function (data){
     res.status(200).json({
         "status": 200,
         "statusText": "OK",
         "message": "All pies retrieved",
-        "data": pies
+        "data": data
+    });
+}, function(err){
+    next(err)
     });
 });
 
