ddaec05ae1f6c249d6ec54cc5547f4a8a1b54ca9
Brian7950 <brianh7950@gmail.com>
2023-05-10 00:32:18 -0600

    added getById method

--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -4,6 +4,7 @@ let fs = require('fs');
 const FILE_NAME = './assets/pies.json';
 
 let pieRepo = {
+    // using promise design pattern
     get: function(resolve, reject) {
         fs.readFile(FILE_NAME, function(err, data){
             if(err){
@@ -11,6 +12,17 @@ let pieRepo = {
             }
             else {
                 resolve(JSON.parse(data));
+            }
+        });
+    },
+    getById: function(id, resolve, reject){
+        fs.readFile(FILE_NAME, function(err, data){
+            if(err){
+                reject(err)
+            }
+            else{
+                let pie= JSON.parse(data).find(p => p.id == id);
+                resolve(pie)
             }
         });
     }
